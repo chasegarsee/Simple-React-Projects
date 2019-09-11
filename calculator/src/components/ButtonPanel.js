1bdce9056e6c4cf5eec5811a014c4f3021eb9ede
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-09-11 13:01:08 -0500

    styling

--- calculator/src/components/ButtonPanel.js
+++ calculator/src/components/ButtonPanel.js
@@ -1,8 +1,7 @@
 import Button from "./Button";
 import React from "react";
 import PropTypes from "prop-types";
-
-//import "./ButtonPanel.css";
+import "./ButtonPanel.css";
 
 export default class ButtonPanel extends React.Component {
   static propTypes = {
